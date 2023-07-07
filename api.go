6a51546cb4e1c77f42d476c4fbf6cf44cc0009e3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-06 20:44:42 -0500

    feat: Add various API functions
    These are basically "utility functions" that are used for button
    callbacks, status readouts, populating the DOM, etc...

--- api.go
+++ api.go
@@ -6,18 +6,40 @@ import (
 )
 
 func boardHandler(w http.ResponseWriter, r *http.Request) {
+  html := ""
+  html += makeHTMLBoard(false, globalBoard)
+  html += makeHTMLBoard(true, globalBoard)
+
+  fmt.Fprintf(w, "%s\n", html)
 }
 
 func validMoveHandler(w http.ResponseWriter, r *http.Request) {
+  from := r.URL.Query().Get("from")
+
+  for rank := 0; rank < 8; rank++ {
+    for file := 0; file < 8; file++ {
+      toSquare := Square{file, rank}
+      if isValidMove(&globalBoard, parseSquare(from), toSquare) {
+        fmt.Fprintf(w, "%c%c\n", 'a' + file, '8' - rank)
+      }
+    }
+  }
 }
 
 func currentFENHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "%s\n", boardToFEN(globalBoard))
 }
 
 func resetHandler(w http.ResponseWriter, r *http.Request) {
+  fen := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  globalBoard = FENToBoard(fen)
+  globalBoard.moveList = []string{}
 }
 
 func moveListHandler(w http.ResponseWriter, r *http.Request) {
+  for n, move := range globalBoard.moveList {
+    fmt.Fprintf(w, "<div class='move-list-item'>%d: %s</div>", n + 1, move)
+  }
 }
 
 func computerMoveHandler(w http.ResponseWriter, r *http.Request) {
