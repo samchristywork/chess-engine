11fa627b6f16e17ad59885e23c195ea757027a21
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-06 19:31:08 -0600

    style: Format file I missed earlier
    In a previous commit, I formatted all of the files that were at the root
    of the repository. The model.go file was not at the root of the
    repository, so it didn't get updated.

--- model/model.go
+++ model/model.go
@@ -3,19 +3,19 @@ package model
 type Piece byte
 
 type Square struct {
-  File int
-  Rank int
+	File int
+	Rank int
 }
 
 type Board struct {
-  Board [8][8]Piece
-  ActiveColor string
-  Castling string
-  EnPassant string
-  HalfMoveClock int
-  FullMoveNumber int
-  LastMove [2]string
-  MoveList []string
-  WhiteAI func(*Board)
-  BlackAI func(*Board)
+	Board          [8][8]Piece
+	ActiveColor    string
+	Castling       string
+	EnPassant      string
+	HalfMoveClock  int
+	FullMoveNumber int
+	LastMove       [2]string
+	MoveList       []string
+	WhiteAI        func(*Board)
+	BlackAI        func(*Board)
 }
